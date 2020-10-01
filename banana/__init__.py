da2e09686d5c2ded4a58e8439cf2ba976b776751
boyska <piuttosto@logorroici.org>
2020-10-01 19:53:10 +0200

    import più facile

--- banana/__init__.py
+++ banana/__init__.py
@@ -1,5 +1,6 @@
 """Top-level package for banana."""
+from .bananalib import *
 
 __author__ = """itec"""
-__email__ = 'itec@ventuordici.org'
-__version__ = '0.1.0'
+__email__ = "itec@ventuordici.org"
+__version__ = "0.1.0"
